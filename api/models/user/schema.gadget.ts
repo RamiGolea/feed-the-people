a6411883fe6524eb3e3afdb1c4a2b50159bf663f
Rami Golea <rami.golea@gmail.com>
2025-03-14 23:31:32 -0400

    First commit

---
+++ api/models/user/schema.gadget.ts
@@ -0,0 +1,58 @@
+import type { GadgetModel } from "gadget-server";
+
+// This file describes the schema for the "user" model, go to https://feed-the-people.gadget.app/edit to view/edit your model in Gadget
+// For more information on how to update this file http://docs.gadget.dev
+
+export const schema: GadgetModel = {
+  type: "gadget/model-schema/v1",
+  storageKey: "YfhwtKQHsIeJ",
+  fields: {
+    email: {
+      type: "email",
+      validations: { required: true, unique: true },
+      storageKey: "OSwCSfGb3tTz",
+    },
+    emailVerificationToken: {
+      type: "string",
+      storageKey: "GXtdSXO0GsZk",
+    },
+    emailVerificationTokenExpiration: {
+      type: "dateTime",
+      includeTime: true,
+      storageKey: "ATGuMP5Z6tZL",
+    },
+    emailVerified: {
+      type: "boolean",
+      default: false,
+      storageKey: "SXW30Vzq_ZuK",
+    },
+    firstName: { type: "string", storageKey: "UTUIWoGy9qLj" },
+    googleImageUrl: { type: "url", storageKey: "OhA9KOcBhVkf" },
+    googleProfileId: { type: "string", storageKey: "_SDPwurg87od" },
+    lastName: { type: "string", storageKey: "YjfcoHFdap0M" },
+    lastSignedIn: {
+      type: "dateTime",
+      includeTime: true,
+      storageKey: "gvTDhqB62Z76",
+    },
+    password: {
+      type: "password",
+      validations: { strongPassword: true },
+      storageKey: "-GE0gbaiLyO6",
+    },
+    resetPasswordToken: {
+      type: "string",
+      storageKey: "UgqchIJt1l7b",
+    },
+    resetPasswordTokenExpiration: {
+      type: "dateTime",
+      includeTime: true,
+      storageKey: "9SoQvowpbu_l",
+    },
+    roles: {
+      type: "roleList",
+      default: ["unauthenticated"],
+      storageKey: "n9hXFvDruzhn",
+    },
+  },
+};
---
+++ web/api.ts
@@ -0,0 +1,5 @@
+// Sets up the API client for interacting with your backend.
+// For your API reference, visit: https://docs.gadget.dev/api/cuhack6-goats
+import { Client } from "@gadget-client/cuhack6-goats";
+
+export const api = new Client();
